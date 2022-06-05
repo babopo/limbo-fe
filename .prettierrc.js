61e5f1704edc5ef8f5887d9f0cd50f77dbca459d
limbo <llb123@live.com>
2022-06-05 11:08:09 +0800

    feat: add pre-comit hookx

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+// const config = require('@ea/eslint-config-ea/prettierrc');
+
+module.exports = {
+    arrowParens: 'avoid',
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    printWidth: 120,
+};
